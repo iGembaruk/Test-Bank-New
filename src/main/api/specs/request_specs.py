fef2d8bdb50b72f2fa00eded6789901eeb245dee
Igor Gembaruk <i.gembaruk@mail.ru>
2026-06-03 05:21:56 +0300

    [lesson2] task2 add models

---
+++ src/main/api/specs/request_specs.py
@@ -0,0 +1,11 @@
+class RequestSpecs:
+
+    @staticmethod
+    def base_headers():
+        return {
+            "Content-Type": "application/json",
+            "accept": "application/json"
+        }
+
+    @staticmethod
+    def auth_headers():...
